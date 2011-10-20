4a7c93cfd633d314c3046a02b14c83d1d45d4ab2
Raphaël Fournier <fournier@lavabit.com>
2011-10-20 17:10:57 +0200

    added pomodoro timer and icon

--- awesome.lua
+++ awesome.lua
@@ -180,6 +180,86 @@ generalvolwidget:buttons(awful.util.table.join(
  awful.button({ }, 5, function () exec("amixer -q sset Master 2dB-") end)
  )
 )
+-- pomodoro timer widget
+pomodoro = {}
+-- tweak these values in seconds to your liking
+pomodoro.pause_duration = 60*5
+pomodoro.work_duration = 60*25
+
+pomodoro.pause_title = "Pause terminée."
+pomodoro.pause_text = "On se remet au travail !"
+pomodoro.work_title = "Pomodoro terminé."
+pomodoro.work_text = "Une petite pause !"
+pomodoro.working = true
+pomodoro.left = pomodoro.work_duration
+pomodoro.widget = widget({ type = "textbox" })
+pomodoro.timer = timer { timeout = 1 }
+
+function pomodoro:settime(t)
+  if t >= 3600 then -- more than one hour!
+    t = os.date("%X", t-3600)
+  else
+    t = os.date("%M:%S", t)
+  end
+  self.widget.text = string.format("<b>%s</b>", t)
+end
+
+function pomodoro:notify(title, text, duration, working)
+  local ico = image(awful.util.getdir("config") .. "/pomodoro.png")
+  naughty.notify {
+    bg = "#880000",
+    fg = "#ffffff",
+    title = title,
+    text = text,
+    timeout = 10, hover_timeout = 1,
+    icon = ico,
+    icon_size = 150,
+    width = 500, position = "top_right", screen = mouse.screen
+  }
+
+  pomodoro.left = duration
+  pomodoro:settime(duration)
+  pomodoro.working = working
+end
+
+pomodoro:settime(pomodoro.work_duration)
+
+pomodoro.widget:buttons(
+  awful.util.table.join(
+    awful.button({ }, 1, function()
+      pomodoro.last_time = os.time()
+      pomodoro.timer:start()
+    end),
+    awful.button({ }, 2, function()
+      pomodoro.timer:stop()
+    end),
+    awful.button({ }, 3, function()
+      pomodoro.timer:stop()
+      pomodoro.left = pomodoro.work_duration
+      pomodoro:settime(pomodoro.work_duration)
+    end)
+))
+
+pomodoro.timer:add_signal("timeout", function()
+  local now = os.time()
+  pomodoro.left = pomodoro.left - (now - pomodoro.last_time)
+  pomodoro.last_time = now
+
+  if pomodoro.left > 0 then
+    pomodoro:settime(pomodoro.left)
+  else
+    if pomodoro.working then
+      pomodoro:notify(pomodoro.work_title, pomodoro.work_text,
+        pomodoro.pause_duration, false)
+    else
+      pomodoro:notify(pomodoro.pause_title, pomodoro.pause_text,
+        pomodoro.work_duration, true)
+    end
+    pomodoro.timer:stop()
+  end
+end)
+
+
 --
 --mpdwidget = widget({ type = "textbox" })
 --vicious.register(mpdwidget, vicious.widgets.mpd, 
@@ -528,6 +608,8 @@ for s = 1, screen.count() do
         spacesep,mailwidget0,
         spacesep,mailwidget1,
         spacesep,mailwidget2,
+        spacesep,
+        pomodoro.widget,
         spacesep,
         --barsep,
         --spacesep,
