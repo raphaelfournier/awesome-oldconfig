7c8bdcadc33996d0da6e9ccfbb72a0f818d5cbb8
Raphaël Fournier <fournier@lavabit.com>
2012-07-30 17:12:42 +0200

    dmenu font and layout on first tab

--- conf/variables.lua
+++ conf/variables.lua
@@ -1,5 +1,6 @@
 local home   = os.getenv("HOME")
-dmenuopts= "-b -i -nf '"..beautiful.fg_normal.."' -nb '"..beautiful.bg_normal.."' -sf '"..beautiful.fg_urgent.."' -sb '"..beautiful.bg_focus.."' -fn '-*-liberation mono-*-r-*-*-*-120-*-*-*-*-*-*'"
+dmenuopts= "-b -i -nf '"..beautiful.fg_normal.."' -nb '"..beautiful.bg_normal.."' -sf '"..beautiful.fg_urgent.."' -sb '"..beautiful.bg_focus.."' -fn '-*-dejavu sans mono-*-r-*-*-*-*-*-*-*-*-*-*'"
+--dmenuopts= "-b -i -nf '"..beautiful.fg_normal.."' -nb '"..beautiful.bg_normal.."' -sf '"..beautiful.fg_urgent.."' -sb '"..beautiful.bg_focus.."' -fn '-*-liberation mono-*-r-*-*-*-120-*-*-*-*-*-*'"
 
 terminal = "urxvtc"
 editor = os.getenv("EDITOR") or "nano"
